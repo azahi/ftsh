36e41c25ed942c3d29feec4eba595e2faa316735
Azat Bahawi <azahi@teknik.io>
2020-02-17 22:28:15 +0300

    Build before running

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env zsh
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DDEBUG=ON || exit 1
+
+cmake --build build --target all || exit 1
+
 ./build/minishell 2> >(while read line; do echo -e "\e[01;31m$line\e[0m" >&2; done)
