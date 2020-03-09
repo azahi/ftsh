249d2a01e73c4a80ce228da3d43626de190ee7a6
pparalax <pparalax@student.21-school.ru>
2020-03-09 20:01:06 +0300

    Norme

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pparalax <pparalax@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/09 19:22:14 by pparalax          #+#    #+#             */
+/*   Updated: 2020/03/09 20:00:34 by pparalax         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <ft.h>
 #include <ft_stdio.h>
 #include <ft_stdlib.h>
@@ -12,69 +24,88 @@
 #define MINISHELL_INPUT_BUFSIZE 8
 #define MINISHELL_INPUT_DELIMITERS "\t\n\v\f\r "
 
-/**
- * Splits input into arguments.
- */
-static char **
-sh_split(char *line, int *linec)
+static void	sh_split_tilda(char **tokens, int i)
 {
-	int bufsize = MINISHELL_INPUT_BUFSIZE;
-	char **tokens = malloc(sizeof (*tokens) * bufsize);
-	char *token = ft_strtok(line, MINISHELL_INPUT_DELIMITERS);
-	int i = 0;
-	while (token)
+	char	*home_expand;
+	size_t	hl;
+	size_t	tl;
+	char	*tmp;
+
+	home_expand = lenv_getenv("HOME");
+	hl = ft_strlen(home_expand);
+	tl = ft_strlen(tokens[i]);
+	tmp = malloc(sizeof(*tmp) * (hl + tl - 1));
+	ft_memcpy(tmp, home_expand, hl);
+	ft_memcpy(tmp + hl, tokens[i] + 1, tl - 1);
+	tmp[hl + tl - 1] = '\0';
+	free(tokens[i]);
+	tokens[i] = tmp;
+}
+
+static void	sh_split_dollar(char **tokens, int i, char *token)
+{
+	char	*expand;
+	char	*expanded;
+	size_t	etl;
+	size_t	el;
+
+	if ((expand = ft_strchr(token, '$')))
 	{
-		/**
-		 * Environment variable expansion.
-		 */
-		char *expand = ft_strchr(token, '$');
-		if (expand)
+		expand++;
+		etl = expand - token - 1;
+		expanded = lenv_getenv(expand);
+		if (expanded)
 		{
-			expand++;
-			size_t etl = expand - token - 1;
-			char *expanded = lenv_getenv(expand);
-			if (expanded)
-			{
-				size_t el = ft_strlen(expanded);
-				tokens[i] = malloc(sizeof (*tokens) * (el + etl));
-				ft_memcpy(tokens[i], token, etl);
-				ft_memcpy(tokens[i] + etl, expanded, el);
-				tokens[i][el + etl] = '\0';
-			}
-			else
-				tokens[i] = ft_strndup(token, etl);
+			el = ft_strlen(expanded);
+			tokens[i] = malloc(sizeof(*tokens) * (el + etl));
+			ft_memcpy(tokens[i], token, etl);
+			ft_memcpy(tokens[i] + etl, expanded, el);
+			tokens[i][el + etl] = '\0';
 		}
 		else
-			tokens[i] = ft_strdup(token);
+			tokens[i] = ft_strndup(token, etl);
+	}
+	else
+		tokens[i] = ft_strdup(token);
+}
 
-		/**
-		 * '~' expansion.
-		 */
+static void	sh_split_bufsize(char ***tokens, int *bufsize)
+{
+	char	**t;
+	int		j;
+
+	*bufsize += MINISHELL_INPUT_BUFSIZE;
+	if (!(t = malloc(sizeof(*t) * *bufsize)))
+		exit(EXIT_FAILURE);
+	j = 0;
+	while (j < *bufsize - MINISHELL_INPUT_BUFSIZE)
+	{
+		t[j] = *tokens[j];
+		j++;
+	}
+	free(*tokens);
+	*tokens = t;
+}
+
+static char	**sh_split(char *line, int *linec)
+{
+	int		bufsize;
+	char	**tokens;
+	char	*token;
+	int		i;
+
+	bufsize = MINISHELL_INPUT_BUFSIZE;
+	tokens = malloc(sizeof(*tokens) * bufsize);
+	token = ft_strtok(line, MINISHELL_INPUT_DELIMITERS);
+	i = 0;
+	while (token)
+	{
+		sh_split_dollar(tokens, i, token);
 		if (*tokens[i] == '~')
-		{
-			char *home_expand = lenv_getenv("HOME");
-			size_t hl = ft_strlen(home_expand);
-			size_t tl = ft_strlen(tokens[i]);
-			char *tmp = malloc(sizeof (*tmp) * (hl + tl - 1));
-			ft_memcpy(tmp, home_expand, hl);
-			ft_memcpy(tmp + hl, tokens[i] + 1, tl - 1);
-			tmp[hl + tl - 1] = '\0';
-			free(tokens[i]);
-			tokens[i] = tmp;
-		}
-
+			sh_split_tilda(tokens, i);
 		i++;
 		if (i >= bufsize)
-		{
-			bufsize += MINISHELL_INPUT_BUFSIZE;
-			char **t;
-			if (!(t = malloc(sizeof (*t) * bufsize)))
-				exit(EXIT_FAILURE);
-			for (int j = 0; j < bufsize - MINISHELL_INPUT_BUFSIZE; j++)
-				t[j] = tokens[j];
-			free(tokens);
-			tokens = t;
-		}
+			sh_split_bufsize(&tokens, &bufsize);
 		token = ft_strtok(NULL, MINISHELL_INPUT_DELIMITERS);
 	}
 	tokens[i] = NULL;
@@ -82,9 +113,9 @@ sh_split(char *line, int *linec)
 	return (tokens);
 }
 
-static void
-sigint_shell()
+static void	sigint_shell(int sig)
 {
+	(void)sig;
 	ufputc(FT_STDOUT, '\n');
 	prompt();
 }
